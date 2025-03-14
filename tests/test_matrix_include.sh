49c74eaea1c392de7bc55ddc72626e1e1828a43d
JonJagger <jon@kosli.com>
2025-03-14 13:42:22 +0000

    Prepare remaining tests

--- tests/test_matrix_include.sh
+++ tests/test_matrix_include.sh
@@ -15,17 +15,29 @@ xtest_new_flow()
 {
   local -r filename="new-flow.json"
   create_matrix_include "${filename}"
+  assert_stdout_equals "$(cat "${my_dir}/expected/${filename}")"
+  assert_stderr_empty
+  assert_status_0
 }
 
 xtest_4_deployments()
 {
   local -r filename="4.json"
   create_matrix_include "${filename}"
+  assert_stdout_equals "$(cat "${my_dir}/expected/${filename}")"
+  assert_stderr_empty
+  assert_status_0
 }
 
 xtest_blue_green_aws_beta()
 {
   local -r filename="blue-green-aws-beta.json"
+  create_matrix_include "${filename}"
+}
+
+xtest_blue_green_aws_prod()
+{
+  local -r filename="blue-green-aws-prod.json"
   create_matrix_include "${filename}"
 }
 
