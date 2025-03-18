53eee33086c8ced1675c7eb34eec5b0ae08b507b
JonJagger <jon@kosli.com>
2025-03-18 09:31:07 +0000

    Start reading diff-json in python script

--- bin/promotions.py
+++ bin/promotions.py
@@ -1,10 +1,32 @@
 #!/usr/local/bin/python3
 
+import json
 import sys
 
 
 def promotions():
-    print(sys.stdin.read())
+    diff = json.loads(sys.stdin.read())
+    incoming = diff["snappish1"]
+    outgoing = diff["snappish2"]
+    common = diff["changed"]["artifacts"] + diff["not-changed"]["artifacts"]
+
+    incoming_flow_names = [artifact["flow"] for artifact in incoming["artifacts"]]
+    outgoing_flow_names = [artifact["flow"] for artifact in outgoing["artifacts"]]
+    common_flow_names = [artifact["flow"] for artifact in common]
+
+    # print(json.dumps(incoming_flow_names, indent=2))
+    # print(json.dumps(outgoing_flow_names, indent=2))
+    # print(json.dumps(common_flow_names, indent=2))
+
+    exit_non_zero_if_mid_blue_green_deployment("incoming", incoming_flow_names + common_flow_names)
+    exit_non_zero_if_mid_blue_green_deployment("outgoing", outgoing_flow_names + common_flow_names)
+
+
+def exit_non_zero_if_mid_blue_green_deployment(kind, flow_names):
+    print(flow_names)
+
+
+
 
 
 if __name__ == "__main__":  # pragma: no cover
--- bin/run_promotions.sh
+++ bin/run_promotions.sh
@@ -3,7 +3,7 @@ set -Eeu
 
 export MY_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-cat ${MY_DIR}/../tests/expected/2.json |
+cat ${MY_DIR}/../tests/diff-snapshots/2.json |
   docker run --rm -i \
     --volume ${MY_DIR}/promotions.py:/usr/src/myapp/promotions.py \
     --workdir /usr/src/myapp \
