3504f0af6d6b2ad1c1cb742896e09719669350f2
JonJagger <jon@kosli.com>
2025-03-18 15:47:41 +0000

    Make deployment job live in main workflow

--- bin/dead/run_promotions.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -Eeu
-
-export MY_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-
-cat ${MY_DIR}/../tests/diff-snapshots/new-flow.json | python3 ${MY_DIR}/promotions.py
-
